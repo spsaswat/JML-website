760db5e3fabbbfbc6fc47cc71cf5ef4b629bf8f2
spsaswat <spsaswat16194@gmail.com>
2024-08-15 15:26:40 +1000

    Updated working in heatmap

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Get buttons
+const expt2Button = document.getElementById("expt2-button");
 const expt3Button = document.getElementById("expt3-button");
 const expt4Button = document.getElementById("expt4-button");
 
 // Add click event listeners
+expt2Button.addEventListener("click", () => {
+    // Navigate to Expt 2 page (update the path if necessary)
+    window.location.href = "./expt2_home.html"; // Update this to the correct path if needed
+});
+
 expt3Button.addEventListener("click", () => {
     // Navigate to Expt 3 page (update the path if necessary)
-    window.location.href = "./expt3_home.html";
+    window.location.href = "./expt3_home.html"; // Update this to the correct path if needed
 });
 
 expt4Button.addEventListener("click", () => {
